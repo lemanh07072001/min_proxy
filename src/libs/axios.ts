76b02a941f041413a1b90a8f49eef1291af268d3
lemanh07072001 <152960889+lemanh07072001@users.noreply.github.com>
2025-09-13 15:14:30 +0700

    up name

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  // baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://api.mktproxy.com/api',
   headers: {
     Accept: 'application/json'
   }
