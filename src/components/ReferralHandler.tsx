c37e095a759bde777947508e45e4ae970bb903f0
lemanh <lemanh.laravel@gmail.com>
2025-10-01 09:23:13 +0700

    up SEO

---
+++ src/components/ReferralHandler.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
+
+const ReferralHandler = () => {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const referralCode = searchParams.get('ref')
+    
+    if (referralCode) {
+      // Lưu referral code vào localStorage
+      localStorage.setItem('referralCode', referralCode)
+      
+      // Có thể thêm logic khác như tracking, analytics, etc.
+      console.log('Referral code detected:', referralCode)
+    }
+  }, [searchParams])
+
+  return null // Component này không render gì
+}
+
+export default ReferralHandler
