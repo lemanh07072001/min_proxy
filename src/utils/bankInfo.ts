714d32d7447dcc4f61ac3b617d325ff718f2cc9b
lemanh <lemanh.laravel@gmail.com>
2025-10-08 16:21:54 +0700

    up

--- src/utils/bankInfo.ts
+++ src/utils/bankInfo.ts
@@ -1,11 +1,13 @@
 export const getBankNumber = (userId: number | string) => {
   const random3 = Math.floor(100 + Math.random() * 900)
 
+  const stringNote = random3 + '.' + userId
+
   return {
     bankCode: '970436',
     bankName: 'Vietcombank',
     accountNumber: '1056968673',
     accountName: 'LUONG VAN THUY',
-    note: `mktproxy ${random3}.${userId}`
+    note: `mktproxy ${stringNote}`
   }
 }
