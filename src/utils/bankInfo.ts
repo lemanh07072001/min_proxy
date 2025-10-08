373755e0390698b19e8cb38e680b1a179064c049
lemanh <lemanh.laravel@gmail.com>
2025-10-08 16:10:17 +0700

    up qr code

--- src/utils/bankInfo.ts
+++ src/utils/bankInfo.ts
@@ -1,9 +1,11 @@
 export const getBankNumber = (userId: number | string) => {
+  const random3 = Math.floor(100 + Math.random() * 900)
+
   return {
     bankCode: '970436',
     bankName: 'Vietcombank',
     accountNumber: '1056968673',
     accountName: 'LUONG VAN THUY',
-    note: `mktproxy ${userId}`
+    note: `mktproxy ${random3}.${userId}`
   }
 }
