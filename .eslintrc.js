70f92167d91eaa866b0b8f78a5be146b751cbce6
lemanh <lemanh.laravel@gmail.com>
2025-12-08 18:56:58 +0700

    up next

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:import/recommended', 'prettier'],
+  extends: ['next/core-web-vitals', 'plugin:import/recommended', 'prettier'],
   rules: {
     'jsx-a11y/alt-text': 'off',
     'react/display-name': 'off',
     'react/no-children-prop': 'off',
     '@next/next/no-img-element': 'off',
     '@next/next/no-page-custom-font': 'off',
-    '@typescript-eslint/consistent-type-imports': 'error',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'off',
     'lines-around-comment': [
       'error',
       {
@@ -85,29 +80,10 @@ module.exports = {
         'newlines-between': 'always-and-inside-groups'
       }
     ],
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          Function: 'Use a specific function type instead',
-          Object: 'Use object instead',
-          Boolean: 'Use boolean instead',
-          Number: 'Use number instead',
-          String: 'Use string instead',
-          Symbol: 'Use symbol instead',
-          any: false,
-          '{}': false
-        }
-      }
-    ]
   },
   settings: {
     react: {
       version: 'detect'
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     'import/resolver': {
       node: {},
@@ -115,14 +91,5 @@ module.exports = {
         project: './tsconfig.json'
       }
     }
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx', 'src/iconify-bundle/*'],
-      rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    }
-  ]
+  }
 }
